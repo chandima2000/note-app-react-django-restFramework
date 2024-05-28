f300d038fed66fa1b0de4fb3f3acdec23ee712e0
chandima2000 <hychandima2000@gmail.com>
2024-05-29 00:05:33 +0530

    Create the UserView functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from rest_framework import generics # type: ignore
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny # type: ignore
+
 
 # Create your views here.
+class CreateUserView(generics.CreateApiView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
