30cbdb891b49118846940895f346a3b6a7691c3a
chandima2000 <hychandima2000@gmail.com>
2024-05-28 23:48:04 +0530

    Create the serializer for User

---
+++ backend/api/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User # type: ignore
+from rest_framework import serializers # type: ignore
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id","username","password"]
+        extra_kwargs = {"password" : {"write_only":True}} 
+        ## This ensures that the password field is write-only, 
+        ## meaning it will not be included in the serialized output (JSON response), 
+        ## but it can be used to create or update a User instance.
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
