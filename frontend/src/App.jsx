8bf16479011d30c5dc9d2902c368779bf8f4bbbc
chandima2000 <hychandima2000@gmail.com>
2024-05-30 01:29:14 +0530

    Register routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom'
+import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+
+
+function logout(){
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register/>
+}
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+          <Route 
+                path='/' 
+                element={
+                <ProtectedRoute>
+                    <Home/>
+                </ProtectedRoute>
+                }
+          />
+
+          <Route 
+                path='/login' 
+                element={<Login/>}
+          />
+          <Route 
+                path='/register' 
+                element={<RegisterAndLogout/>}
+          />
+          <Route 
+                path='*' 
+                element={<NotFound/>}
+          />
+      </Routes>
+    </BrowserRouter>
   )
 }
