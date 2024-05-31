8f1b0d3e07ce2c9bf95fd81b97d5b1b4a3fa9760
chandima2000 <hychandima2000@gmail.com>
2024-05-31 17:58:00 +0530

    Style the Note component

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/note.css';
 
 export default function Notes({note,onDelete}) {
     const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
   return (
     <div className="note-container">
-            <p className="note-title">{note.title}</p>
-            <p className="note-content">{note.content}</p>
-            <p className="note-date">{formattedDate}</p>
+        <div className="flex-container-1">
+        <p><span className='note-subs'>Title :</span> <span className="note-title">
+            {note.title}   </span></p>
+            <p><span className='note-subs'>Content : </span><span className="note-content">
+            {note.content}  </span></p>
+            <p><span className='note-subs'>Published Date: </span><span className="note-date">
+            {formattedDate}  </span></p>
+        </div>
+        <div className="flex-container-2">
             <button className="delete-button" onClick={() => onDelete(note.id)}>
                 Delete
             </button>
+        </div>
+            
         </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -52,34 +52,36 @@ function Home() {
 
     return (
         <div>
-            <div>
+            <div className="flex-content-1">
                 <h2>Notes</h2>
                 {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id}/>)}
             </div>
-            <h2>Create a Note</h2>
-            <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <label htmlFor="content">Content:</label>
-                <br />
-                <textarea
-                    id="content"
-                    name="content"
-                    required
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                ></textarea>
-                <br />
-                <input type="submit" value="Submit"></input>
-            </form>
+            <div className="flex-content-2">
+             <h2>Create a Note</h2>
+                <form onSubmit={createNote}>
+                    <label htmlFor="title">Title:</label>
+                    <br />
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        required
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                    <label htmlFor="content">Content:</label>
+                    <br />
+                    <textarea
+                        id="content"
+                        name="content"
+                        required
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    ></textarea>
+                    <br />
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
         </div>
     );
 }
