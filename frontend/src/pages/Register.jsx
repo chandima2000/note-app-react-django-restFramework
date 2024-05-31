cd63cedce3bb29cf11cfa8729e7e757be7b80da8
chandima2000 <hychandima2000@gmail.com>
2024-05-31 17:12:07 +0530

    Create the Home page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,6 @@ import Form from '../components/Form';
 
 export default function Register() {
   return (
-    <Form route="/api/user/register/" method={"register"}/>
+    <Form route="/api/user/register/" method="register"/>
   )
 }
